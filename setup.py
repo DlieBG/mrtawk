24f2966b5a8100d650b709b21d8669ac9f843950
Benedikt Schwering <mail@bschwer.ing>
2024-12-06 08:12:07 +0100

    feat(project): setup project

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+mrtawk - Advanced Anomaly Detection in Internet Routing
+Copyright (C) 2024 Benedikt Schwering
+
+This software is distributed under the terms of the MIT license.
+It can be found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+Author Benedikt SCHWERING <bes9584@thi.de>
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name='mrtawk',
+    version='0.1.0',
+    description='',
+    url='https://git.univ.leitwert.net/imprj/01-bgp-testbed/mrtawk',
+    author='Benedikt Schwering',
+    author_email='bes9584@thi.de',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pydantic',
+        'click',
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mrtawk=src.main:cli',
+        ],
+    },
+)
---
+++ src/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+mrtawk - Advanced Anomaly Detection in Internet Routing
+Copyright (C) 2024 Benedikt Schwering
+
+This software is distributed under the terms of the MIT license.
+It can be found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+Author Benedikt SCHWERING <bes9584@thi.de>
+"""
+from rich import print
+import click
+
+@click.command()
+def cli():
+    print("jo")
